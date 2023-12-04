e4be25fcf923157ac5669f5639cb61b23e38abc1
PauloAlecio <46734970+PauloAlecio@users.noreply.github.com>
2023-12-04 20:58:08 -0300

    Primeiro commit

---
+++ src/main/java/com/pauloalecio/springsecurity/SpringSecurityTestApplication.java
@@ -0,0 +1,17 @@
+package com.pauloalecio.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTestApplication
+{
+
+    public static void main ( String[] args ) {
+        SpringApplication.run(
+            SpringSecurityTestApplication.class,
+            args
+        );
+    }
+
+}
---
+++ src/test/java/com/pauloalecio/springsecurity/SpringSecurityTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.pauloalecio.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTestApplicationTests
+{
+
+    @Test
+    void contextLoads () {
+    }
+
+}
